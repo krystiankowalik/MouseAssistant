f8e0eac5d487491ac75adb6a7eae62c322f491c5
Krystian Kowalik <krystiankowalik1991@gmail.com>
2018-09-08 00:05:45 +0200

    Tray added

--- src/main/kotlin/com/github/krystiankowalik/mouseassistant/MyApp.kt
+++ src/main/kotlin/com/github/krystiankowalik/mouseassistant/MyApp.kt
@@ -3,9 +3,10 @@ package com.github.krystiankowalik.mouseassistant
 import com.github.krystiankowalik.mouseassistant.app.Styles
 import com.github.krystiankowalik.mouseassistant.view.MainView
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.scene.image.Image
 import javafx.stage.Stage
-import tornadofx.App
+import tornadofx.*
 
 object MyApp {
 
@@ -13,6 +14,29 @@ object MyApp {
         override fun start(stage: Stage) {
             super.start(stage)
             stage.icons += Image("Hamster.png")
+
+            trayicon(resources.stream("/Hamster.png")) {
+                setOnMouseClicked(fxThread = true) {
+                    FX.primaryStage.show()
+                    FX.primaryStage.toFront()
+                }
+
+                menu("MyApp") {
+                    item("Show...") {
+                        setOnAction(fxThread = true) {
+                            FX.primaryStage.show()
+                            FX.primaryStage.toFront()
+                        }
+                    }
+                    item("Exit") {
+                        setOnAction(fxThread = true) {
+                            Platform.exit()
+                        }
+                    }
+                }
+
+
+            }
         }
     }
 
